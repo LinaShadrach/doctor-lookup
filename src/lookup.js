bf851016ce84e533da42b038e0568227d9426f02
russell hofvendahl <rhofvendahl@gmail.com>
2018-03-23 15:37:31 -0700

    refactor, fix website links

--- src/lookup.js
+++ src/lookup.js
@@ -1,19 +1,41 @@
-function toPaddedString(int) {
-  if (int < 10) return "0" + int;
-  else return int.toString();
+function listDoctors(data) {
+  data.forEach(function(doctor) {
+    let firstName = doctor.profile.first_name;
+    let lastName = doctor.profile.last_name;
+    let address = doctor.practices[0].visit_address;
+    let formattedAddress = address.street+", "+address.city+" "+address.state+", "+address.zip;
+    let phone = doctor.practices[0].phones[0].number;
+    let website = doctor.practices[0].website;
+    let acceptingNew = doctor.practices[0].accepts_new_patients;
+
+    let toAppend = "<li>"
+    if (website) toAppend += "<h4><a href='"+website+"'>Dr. " +firstName+" "+lastName+"</a></h4>";
+    else toAppend += "<h4>Dr. " +firstName+" "+lastName+"</h4>"
+    toAppend += "<p>"+formattedAddress+"</p>";
+    toAppend += "<p>"+phone+"</p>";
+    if(acceptingNew) toAppend += "<p>Accepting new patients</p>";
+    else toAppend += "<p>Not accepting new patients</p>";
+    toAppend += "</li>";
+
+    $('#doctors').append(toAppend);
+  });
 }
 
 export function lookup({firstName, lastName, specialty}) {
-  const date = Date.now();
-  const formattedDate = date.getFullYear()}+"-"+toPaddedString(date.getMonth())+"-"+toPaddedString(date.getDate())
+  let url = "https://api.betterdoctor.com/2016-03-01/doctors?";
+  if (firstName) url += "first_name=" + firstName + "&";
+  if (lastName) url += "last_name=" + lastName + "&";
+  if (specialty) url += "specialty_uid=" + specialty + "&";
+  url += "location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=";
+  url += process.env.exports.apiKey;
 
-  let request = new XMLHttpRequest();
-
-  let url = "https://api.betterdoctor.com/&{formattedDate}/doctors?"
-  if (firstName) url += "first_name=${firstName}&"
-  if (lastName) url += "last_name=${lastName}&"
-  if (specialty) url += "specialty_uid=${specialty}&"
-  url += "location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=9a04c97b866b15e5f0e8a24fa4252742"
-
-  $.get(url).then(function)
+  $.get(url).then(function(response) {
+    if (response.data.length == 0) {
+      $("#response").text("No matches found");
+    } else {
+      listDoctors(response.data);
+    }
+  }).fail(function(error) {
+    $("#response").text("Error: " + error.statusText);
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,54 +1,17 @@
-// import './styles.css';
-// import { pingPong } from './ping-pong';
-// import { lookup } from './lookup';
-
-function listDoctors(data) {
-  data.forEach(function(doctor) {
-    let firstName = doctor.profile.first_name;
-    let lastName = doctor.profile.last_name;
-    let address = doctor.practices[0].visit_address;
-    let formattedAddress = address.street+", "+address.city+" "+address.state+", "+address.zip;
-    let phone = doctor.practices[0].phones[0].number;
-    let website = doctor.practices[0].website;
-    let acceptingNew = doctor.practices[0].accepts_new_patients;
-
-    let toAppend = "<li><h4><a href='"+website+"'>Dr. " +firstName+" "+lastName+"</a></h4>";
-    toAppend += "<p>"+formattedAddress+"</p>";
-    toAppend += "<p>"+phone+"</p>";
-    if(acceptingNew) toAppend += "<p>Accepting new patients</p>";
-    else toAppend += "<p>Not accepting new patients</p>";
-    toAppend += "</li>";
-
-    $('#doctors').append(toAppend);
-  });
-}
+import './styles.css';
+import { lookup } from './lookup';
 
 $(document).ready(function() {
   $('#lookup-form').submit(function(event) {
     event.preventDefault();
 
-    $("#response").text("")
+    $("#response").text("");
     $('#doctors').empty();
 
-    const firstName = $('#first-name').val();
-    const lastName = $('#last-name').val();
-    const specialty = $('#specialty').val();
-
-    let url = "https://api.betterdoctor.com/2016-03-01/doctors?"
-    if (firstName) url += "first_name=" + firstName + "&"
-    if (lastName) url += "last_name=" + lastName + "&"
-    if (specialty) url += "specialty_uid=" + specialty + "&"
-    url += "location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=";
-    url += process.env.exports.apiKey;
-
-    $.get(url).then(function(response) {
-      if (response.data.length == 0) {
-        $("#response").text("No matches found");
-      } else {
-        listDoctors(response.data);
-      }
-    }).fail(function(error) {
-      $("#response").text("Error: " + error.statusText)
-    })
+    lookup({
+      firstName: $('#first-name').val(),
+      lastName: $('#last-name').val(),
+      specialty: $('#specialty').val()
+    });
   });
 });
